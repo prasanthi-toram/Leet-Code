cde3c4334828fe93a2c20973bf068a427cf64e3b
Prasanthi Mounika Toram <153417595+prasanthi-toram@users.noreply.github.com>
2024-11-14 11:27:24 -0500

    Time: 4 ms (91.19%), Space: 16.8 MB (87.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,18 @@
 class Solution:
+    # def isPalindrome(self, s: str) -> bool:
+    #     newstr=""
+    #     for strng in s:
+    #         if strng.isalnum():
+    #             newstr += strng.lower()
+    #     return newstr==newstr[::-1]
     def isPalindrome(self, s: str) -> bool:
-        newstr=""
-        for strng in s:
-            if strng.isalnum():
-                newstr += strng.lower()
-        return newstr==newstr[::-1]
+        i,j = 0,len(s)-1
+        while i<j:
+            while i<j and  not s[i].isalnum():
+                i+=1
+            while i<j and  not s[j].isalnum():
+                j-=1        
+            if s[i].lower()!=s[j].lower():
+                return False
+            i,j=i+1,j-1
+        return True
