114a72c92f5199a103bafe56176c5717effec9be
Prasanthi Mounika Toram <153417595+prasanthi-toram@users.noreply.github.com>
2024-11-20 19:00:44 -0500

    Time: 0 ms (100%), Space: 16.4 MB (91.07%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -3,8 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+# class Solution:
+    # def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -13,16 +13,28 @@ class Solution:
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        nodeslist=[]
-        curr=head
-        while curr:
-            nodeslist.append(curr)
-            curr=curr.next
-        # nodeslist.append(ListNode())
-        if n == len(nodeslist):
-            return head.next
-        nodeslist[-n-1].next=nodeslist[-n].next
-        return head
+        # nodeslist=[]
+        # curr=head
+        # while curr:
+        #     nodeslist.append(curr)
+        #     curr=curr.next
+        # # nodeslist.append(ListNode())
+        # if n == len(nodeslist):
+        #     return head.next
+        # nodeslist[-n-1].next=nodeslist[-n].next
+        # return head
+
+        dummy = ListNode(0, head)
+        fast=head
+        slow=dummy
+
+        for i in range(n):
+            fast=fast.next
+        while fast:
+            fast=fast.next
+            slow=slow.next
+        slow.next=slow.next.next
+        return dummy.next
 
 
         
